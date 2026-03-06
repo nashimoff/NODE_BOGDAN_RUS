701780a74b8b5b4a4c7de62bb1590c64faecceb7
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-03-06 10:44:47 +0600

    ф

--- fibonacci-with-cache.js
+++ fibonacci-with-cache.js
@@ -2,17 +2,23 @@
 
 setTimeout(() => console.log('Timeout'), 0)
 
+const cache = new Map()
+
 function fib(n) {
     return new Promise((resolve, reject) => {
         if (n === 0 || n === 1) {
             return resolve(n)
         }
+        if (cache.has(n)) {
+            return resolve(cache.get(n))
+        }
         setImmediate(() => 
-            Promise.all([fib(n-1), fib(n-2)])   
-            .then(([fib1, fib2]) => resolve(fib1 + fib2)) 
-         ) 
+            fib(n-1).then((fib1) => fib(n-2).then((fib2) => {
+                cache.set(n, fib1 + fib2)
+                resolve(fib1 + fib2)
+            }))             
+        ) 
    }) 
 }
 
-// Heap out of memory NURLAN kanday?
-fib(40 ).then((res) => console.log(res)) 
+fib(100000).then((res) => console.log(res)) 
