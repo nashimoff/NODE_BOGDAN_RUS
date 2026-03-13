16e760829dbda882a6fb3bd5d53ad308619c7ce2
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-03-13 15:08:50 +0600

    q

--- 04-es6-modules/index.mjs
+++ 04-es6-modules/index.mjs
@@ -1,8 +1,13 @@
 import { season, temperature } from "./named-exports.mjs";
 import { isRaining, humidity } from "./inline-exports.mjs";
+import getDataFromServer from "./default-export.mjs";
 
 console.log(season);
 console.log(temperature);
 
 console.log(isRaining);
 console.log(humidity);
+
+getDataFromServer('https://jsonplaceholder.typicode.com/posts/1')
+    .then((post) => console.log(post))
+    .catch((err) => console.error(err))
