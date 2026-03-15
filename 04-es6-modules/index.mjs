214075a03253a01c5de831a66d69584ddd555966
Nurlan Ashimov <nashimoff@gmail.com>
2026-03-15 14:32:02 +0600

    a

--- 04-es6-modules/index.mjs
+++ 04-es6-modules/index.mjs
@@ -1,6 +1,7 @@
 import { season, temperature } from "./named-exports.mjs";
 import { isRaining, humidity } from "./inline-exports.mjs";
 import getDataFromServer from "./default-export.mjs";
+import DEFAULT_SERVER, { USERNAME as MY_USERNAME, PASSWORD as MY_PASSWORD } from "./mixed-exports.mjs";
 
 console.log(season);
 console.log(temperature);
@@ -10,4 +11,7 @@ console.log(humidity);
 
 getDataFromServer('https://jsonplaceholder.typicode.com/posts/1')
     .then((post) => console.log(post))
-    .catch((err) => console.error(err))
+    .catch((err) => console.error(err));
+
+console.log(DEFAULT_SERVER);
+console.log(MY_USERNAME, MY_PASSWORD);
