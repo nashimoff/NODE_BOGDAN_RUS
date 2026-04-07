12f6bafaa41f87ee9d784c3aa18788c9202a53d9
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-04-07 14:38:57 +0600

    a

--- 05-fs/promises-fs.js
+++ 05-fs/promises-fs.js
@@ -2,20 +2,8 @@ const fs = require("fs/promises");
 
 fs.writeFile('./first.txt', 'First file text')
     .then(() => console.log('File first.txt was written'))
-
-fs.writeFile("./first.txt", "First file text", (err) => {
-  if (err) console.log(err);
-  else {
-    console.log("File first.txt was written");
-    fs.appendFile("./first.txt", "\nOne more line", (err) => {
-      if (err) console.log(err);
-      else {
-        console.log("Appended text to the first.txt file");
-        fs.rename("./first.txt", "./renamed-first.txt", (err) => {
-          if (err) console.log(err);
-          else console.log("File was renamed");
-        });
-      }
-    });
-  }
-});
+    .then(() => fs.appendFile("./first.txt", "\nOne more line"))
+    .then(() => console.log("Appended text to the first.txt file"))
+    .then(() => fs.rename("./first.txt", "./renamed-first.txt"))
+    .then(()=> console.log("File was renamed"))
+    .catch((err) => console.log(err))
