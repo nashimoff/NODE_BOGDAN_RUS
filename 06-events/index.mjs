423de034cfda77b94b26468e14c990abe03efb77
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-04-09 16:11:16 +0600

    a

--- 06-events/index.mjs
+++ 06-events/index.mjs
@@ -1,9 +1,10 @@
-import { EventEmitter } from 'events';
+import { EventEmitter } from "events";
 
 const myEmitter = new EventEmitter();
 
-myEmitter.on('timeout', () => {
-    console.log('Timeout event!')
-})
+myEmitter.on("timeout", () => {
+  console.log("Timeout event!");
+});
 
-myEmitter.emit('timeout')
+setTimeout(() => myEmitter.emit("timeout"), 1000);
+setTimeout(() => myEmitter.emit("timeout"), 2000);
