c0edf2f51a5a9f24fbdf847b35ce5d0f77f9aa26
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-04-10 14:48:10 +0600

    a

--- 06-events/index.mjs
+++ 06-events/index.mjs
@@ -2,9 +2,18 @@ import { EventEmitter } from "events";
 
 const myEmitter = new EventEmitter();
 
-myEmitter.on("timeout", () => {
-  console.log("Timeout event!");
+myEmitter.on("timeout", (secondsQty) => {
+  console.log(`Timeout event in ${secondsQty} seconds!`);
 });
 
-setTimeout(() => myEmitter.emit("timeout"), 1000);
-setTimeout(() => myEmitter.emit("timeout"), 2000);
+setTimeout(() => myEmitter.emit("timeout", 1), 1000);
+setTimeout(() => myEmitter.emit("timeout", 2), 2000);
+
+myEmitter.once('singleEvent', () => {
+  console.log('Single event occurred')
+})
+
+
+// Listener function will be called only once!
+setTimeout(() => myEmitter.emit("singleEvent", 1), 500);
+setTimeout(() => myEmitter.emit("singleEvent", 2), 1500);
