ebfef5236df42092b7a2cf20cbc6c76c1865d9bd
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-04-14 16:57:35 +0600

    A

---
+++ 06-events/fs-events.mjs
@@ -0,0 +1,6 @@
+fs.writeFileSync("./first.txt", "First file text");
+console.log("File first.txt was written");
+fs.appendFileSync("./first.txt", "\nOne more line");
+console.log("Appended text to the first.txt file");
+fs.renameSync("./first.txt", "./renamed-first.txt");
+console.log("File was renamed");
