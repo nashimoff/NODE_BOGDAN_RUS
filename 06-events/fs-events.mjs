866ae49de4b9c00a16ac5fe1db2fef2f0a1afdfc
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-04-15 13:39:40 +0600

    4

--- 06-events/fs-events.mjs
+++ 06-events/fs-events.mjs
@@ -5,13 +5,20 @@ const fileEmitter = new EventListener();
 
 fileEmitter.on("writeComplete", () => {
   console.log("File first.txt was written");
-  fs.appendFile("./first.txt", "\nOne more line", () => {});
+  fs.appendFile("./first.txt", "\nOne more line", () => {
+    fileEmitter.emit('appendComplete')
+  });
 });
+
+fileEmitter.on('appendComplete', () => {
+
+})
+
 
 fs.writeFile("./first.txt", "First file text", () => {
     fileEmitter.emit('writeComplete')
 });
 
-console.log("Appended text to the first.txt file");
+
 fs.rename("./first.txt", "./renamed-first.txt");
 console.log("File was renamed");
