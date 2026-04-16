ba62fdbe0b24c1055aba48f352022bf6b14a8707
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-04-16 15:05:41 +0600

    a

--- 06-events/class-events.mjs
+++ 06-events/class-events.mjs
@@ -1,19 +1,21 @@
-import EventEmitter from events;
+import EventEmitter from "events";
 
 class Post extends EventEmitter {
-    constructor(author, text) {
-        super()
-        this.author = author
-        this.text = text
-        this.likeQty = 0
-    }
+  constructor(author, text) {
+    super();
+    this.author = author;
+    this.text = text;
+    this.likesQty = 0;
+  }
 
-    like() {
-        this.likesQty += 1
-    }
+  like() {
+    this.likesQty += 1;
+  }
 }
 
-const myPost = new Post('Bogdan', 'My greate post!')
-console.log(myPost.author)
-console.log(myPost.author)
-console.log(myPost.author)
+const myPost = new Post("Bogdan", "My greate post!");
+console.log(myPost.author);
+console.log(myPost.text);
+console.log(myPost.likesQty);
+myPost.like();
+console.log(myPost.likesQty);
