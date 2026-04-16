12ca6890c3932472020a938a0ac3c2ea6766e5dc
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-04-16 17:20:52 +0600

    a

--- 06-events/class-events.mjs
+++ 06-events/class-events.mjs
@@ -8,19 +8,21 @@ class Post extends EventEmitter {
     this.likesQty = 0;
   }
 
-  like() {
+  like(username) {
     this.likesQty += 1;
-    this.emit('likePost')
+    this.emit("likePost", username);
   }
 }
 
 const myPost = new Post("Bogdan", "My greate post!");
-myPost.on('likePost', () => {
-    console.log('Someone liked your post!')
-})
+myPost.on("likePost", (username) => {
+  console.log(`${username} liked your post!`);
+});
 
-console.log(myPost.author);
-console.log(myPost.text);
-console.log(myPost.likesQty);
-myPost.like();
-console.log(myPost.likesQty);
+// console.log(myPost.author);
+// console.log(myPost.text);
+// console.log(myPost.likesQty);
+myPost.like("alice");
+setTimeout(() => myPost.like("bob"), 1000);
+setTimeout(() => myPost.like("alex"), 2000);
+// console.log(myPost.likesQty);
