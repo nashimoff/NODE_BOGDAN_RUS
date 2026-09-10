b47a1122bc9a5cdcca824a8730422e266fff3199
nashimoff <nurlan.ashimoff@gmail.com>
2026-09-10 17:25:39 +0600

    A

---
+++ 08-http/inde.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+const PORT = 5000
+
+const server = http.createServer((req, res) => {
+    console.log(req)
+    res.end('Greetings from the HTTP server!')
+})
+
+server.listen(PORT, () => {
+    console.log(`Server was launched on port ${PORT}`)
+})
+
