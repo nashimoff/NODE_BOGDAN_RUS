0663eb36da6c28a55075c7a7de399453bd599e78
nashimoff <nurlan.ashimoff@gmail.com>
2026-09-10 17:35:21 +0600

    a

--- 08-http/index.js
+++ 08-http/index.js
@@ -4,10 +4,10 @@ const PORT = 5000
 
 const server = http.createServer((req, res) => {
     console.log(req)
-    res.end('Greetings from the HTTP server!')
-})
+    res.end('Greetings from the HTTP server!');
+});
 
 server.listen(PORT, () => {
-    console.log(`Server was launched on port ${PORT}`)
-})
+    console.log(`Server was launched on port ${PORT}`);
+});
 
