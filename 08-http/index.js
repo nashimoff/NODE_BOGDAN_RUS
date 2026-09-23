f38e64862867bf8fac20d3a2cc0d4e144388ecdb
nashimoff <nurlan.ashimoff@gmail.com>
2026-09-23 20:38:31 +0600

    a

--- 08-http/index.js
+++ 08-http/index.js
@@ -3,7 +3,8 @@ const http = require('http')
 const PORT = 5000
 
 const server = http.createServer((req, res) => {
-    console.log(req)
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
     res.end('Greetings from the HTTP server!');
 });
 
@@ -11,3 +12,4 @@ server.listen(PORT, () => {
     console.log(`Server was launched on port ${PORT}`);
 });
 
+//
