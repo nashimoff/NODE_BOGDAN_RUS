8aed7256ff40ab60201a04958ab87d5dd079a7f9
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-09-25 17:02:09 +0600

    a

--- 08-http/index.js
+++ 08-http/index.js
@@ -3,13 +3,20 @@ const http = require('http')
 const PORT = 5000
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
+    if (req.url === '/http') {
+        res.statusCode = 200
     res.setHeader('Content-Type', 'text/html')
-    res.end('<h1>Greetings from the HTTP server!</h1>');
+    return res.end('<h1>Greetings from the HTTP server!</h1>');
+    } 
+    if (req.url == '/text') {
+        res.statusCode = 200
+        res.setHeader('Content-Type','text/plain')
+        return res.end('This is plain text')
+    }
 });
 
 server.listen(PORT, () => {
     console.log(`Server was launched on port ${PORT}`);
 });
 
-//
+
